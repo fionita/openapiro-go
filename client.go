ed48d8f131443324aae3bfbe3611f6f86a32126f
Florin Ionita <florin.ionitza@gmail.com>
2017-05-09 22:17:43 +0300

    remove unused interface

--- client.go
+++ client.go
@@ -15,10 +15,6 @@ type ApiClient struct {
 	conf *Config
 }
 
-type Client interface {
-	Company(cif string) (*CompanyResponse, error)
-}
-
 func Init(conf *Config) (*ApiClient, error) {
 	if conf.Token == "" {
 		return nil, fmt.Errorf("%v", "No apiKey provided!")
